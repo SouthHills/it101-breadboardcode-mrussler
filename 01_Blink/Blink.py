6544170f4a9077e58538ce25f4ce5e3631647b3d
mrussler <mrussler46@southhills.edu>
2024-04-11 15:24:31 -0400

    Got it to work!

--- 01_Blink/Blink.py
+++ 01_Blink/Blink.py
@@ -2,9 +2,10 @@ from gpiozero import LED as LEDClass # Alias
 import time
 
 LED = LEDClass(17)  # define led
+DED = LEDClass(27)
 
 def loop():
-    global LED
+    global LED, DED
     while True:
         LED.on() 
         print ("led turned on >>>") # print information on terminal
@@ -12,11 +13,18 @@ def loop():
         LED.off()
         print ("led turned off <<<")
         time.sleep(1)
+        DED.on() 
+        print ("led turned on >>>") # print information on terminal
+        time.sleep(2)
+        DED.off()
+        print ("led turned off <<<")
+        time.sleep(2)
         
 def destroy():
     global LED
     # Release resources
     LED.close()
+    DED.close()
 
 if __name__ == "__main__":    # Program start point
     print("Program is starting ... \n")
